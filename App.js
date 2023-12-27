3a9ea08ac26d4a7861d2f090ed8cd171b620ae06
99Yogeshh <yogesh.hola9@outlook.com>
2023-12-27 12:20:31 +0530

    in app

--- App.js
+++ App.js
@@ -11,6 +11,10 @@ const parent=React.createElement('div',{id:'parent'},[
         React.createElement('h1',{},'This is h1 Tag3'),
         React.createElement('h2',{},'This is h2 Tag3')
     ]),
+    React.createElement('div',{id:'child'},[
+        React.createElement('h1',{},'This is h1 Tag3'),
+        React.createElement('h2',{},'This is h2 Tag3')
+    ]),
 ])
 const root=ReactDOM.createRoot(document.getElementById('root'))
 root.render(parent)
