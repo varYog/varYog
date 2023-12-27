5caebda474574c3b930a1c0f032189567d9e54a7
99Yogeshh <yogesh.hola9@outlook.com>
2023-12-27 14:22:04 +0530

    done

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ const parent=React.createElement('div',{id:'parent'},[
     ]),
     React.createElement('div',{id:'child'},[
         React.createElement('h1',{},'This is h1 Tag2'),
+        
         React.createElement('h2',{},'This is h2 Tag2')
     ]),
     React.createElement('div',{id:'child'},[
