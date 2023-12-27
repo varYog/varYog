80d79a326956fac7aadfac51eb2f7e24de7d0314
99Yogeshh <yogesh.hola9@outlook.com>
2023-12-27 11:52:36 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent=React.createElement('div',{id:'parent'},[
+    React.createElement('div',{id:'child'},[
+        React.createElement('h1',{},'This is h1 Tag1'),
+        React.createElement('h2',{},'This is h2 Tag1')
+    ]),
+    React.createElement('div',{id:'child'},[
+        React.createElement('h1',{},'This is h1 Tag2'),
+        React.createElement('h2',{},'This is h2 Tag2')
+    ]),
+    React.createElement('div',{id:'child'},[
+        React.createElement('h1',{},'This is h1 Tag3'),
+        React.createElement('h2',{},'This is h2 Tag3')
+    ]),
+])
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
